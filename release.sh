724988fbda5da1b68b181a2e87845c3daa24ad41
Kevin DC <kevindelcastillo@ravn.co>
2023-12-12 13:24:58 -0500

    chore: Check if a version tag already exists in release.sh

--- release.sh
+++ release.sh
@@ -2,6 +2,11 @@
 
 version="$1"
 
+if git tag | grep -q "^${version}$"; then
+  echo "Tag for version ${version} already exists"
+  exit 1
+fi
+
 if [[ -n $(git status --porcelain) ]]; then
   echo "Git workspace is not empty, aborting..."
   exit 1
