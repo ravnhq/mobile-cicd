29f28ba3ceddfcd06275125cafe3acbe1d26f6e8
Kevin DC <kevindelcastillo@ravn.co>
2023-12-13 13:31:41 -0500

    fix: Fix unhandled nil cases

--- lanes/util.rb
+++ lanes/util.rb
@@ -1,15 +1,19 @@
-# @param [String] value
+# frozen_string_literal: true
+
+# @param [String, NilClass] value
 # @param [TrueClass, FalseClass] default
 # @return [TrueClass, FalseClass]
 def parse_boolean(value, default)
-  value = value.downcase.strip
+  value = value&.downcase&.strip
   blank?(value) ? default : %w[yes true 1].include?(value)
 end
 
-# @param [Object] obj
+# @param [Object, NilClass] obj
 # @return [TrueClass, FalseClass]
 def blank?(obj)
+  # noinspection RubyNilAnalysis
   return obj.strip.empty? if obj.is_a?(String)
 
+  # noinspection RubyNilAnalysis
   obj.respond_to?(:empty?) ? obj.empty? : !obj
 end
